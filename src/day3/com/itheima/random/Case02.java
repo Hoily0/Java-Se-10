d519e6f9e1752ce45200eb1a97cdf0d0d0e216ad
Li Bian <libianbian453.gmail.com>
2024-04-13 17:47:03 +0800

    Changes

--- src/day3/com/itheima/random/Case02.java
+++ src/day3/com/itheima/random/Case02.java
@@ -1,4 +1,8 @@
-package com.itheima.random;
+package day3.com.itheima.random;
+
+import javax.print.attribute.standard.PrinterMakeAndModel;
+import java.util.Random;
+import java.util.Scanner;
 
 /**
  * 猜数字游戏
@@ -6,6 +10,32 @@ package com.itheima.random;
 public class Case02 {
     public static void main(String[] args) {
         //需求： 随机生成一个1-100之间的数据，提示用户猜测，猜大提示过大，猜小提示过小，直到猜中结束游戏。
+        Scanner sc = new Scanner(System.in);
+        Random rd = new Random();
+
+
+        int key = rd.nextInt(100) + 1;
+
+        System.out.println("请输入数据");
+        int num = sc.nextInt();
+
+        while (num != key){
+            if (num > key){
+                System.out.println("猜大了");
+                num = sc.nextInt();
+            }else if(num < key){
+                System.out.println("猜小了");
+                num = sc.nextInt();
+            }
+        }
+
+            System.out.println("猜对了，答案是" +key+ "游戏终止");
+
+
+
+
+
+
 
 
     }
