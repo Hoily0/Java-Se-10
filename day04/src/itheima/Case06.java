1570131314fad18f4f8bef244e867eae683df950
Li Bian <libianbian453.gmail.com>
2024-04-16 21:19:42 +0800

    Changes

--- day04/src/itheima/Case06.java
+++ day04/src/itheima/Case06.java
@@ -5,28 +5,28 @@ import java.util.Scanner;
 public class Case06 {
     public static void main(String[] args) {
         //需求:某歌唱比赛，需要开发一个系统：可以录入6名评委的打分，录入完毕后立即输出平均分做为选手得分。
+        Scanner sc = new Scanner(System.in);
+
+        int sum = 0;
+        for (int j = 0; j < 6; j++) {
+
+            sum += (int) sc.nextDouble();
+        }
+        System.out.println(sum * 1.0 / 6);
+    }
+
 //        Scanner sc= new Scanner(System.in);
 //
-//        int sum = 0;
-//        for (int j = 0; j < 6; j++) {
+//        double[] fraction = new double[6];
+//        double sum = 0;
 //
-//            sum += sc.nextDouble();
+//        for (int i = 0; i < fraction.length; i++) {
+//
+//            fraction[i] = sc.nextDouble();
+////            System.out.println(fraction[i]);
+//            sum += fraction[i];
 //        }
 //        System.out.println(sum * 1.0 / 6 );
 
-
-        Scanner sc= new Scanner(System.in);
-
-        double[] fraction = new double[6];
-        double sum = 0;
-
-        for (int i = 0; i < fraction.length; i++) {
-
-            fraction[i] = sc.nextDouble();
-//            System.out.println(fraction[i]);
-            sum += fraction[i];
-        }
-        System.out.println(sum * 1.0 / 6 );
-
     }
-}
+
